dedcc8543e287b899b7801e4d84cd8c218a08d21
Willem Kromdijk <willemkromdijk@alliander.com>
2023-03-01 12:04:16 +0100

    '

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const http = require("http");
 
 const port = 4000
 app.get("/", (req, res) => {
-    res.send("Hello Willem")
+    res.send("Hello Willem & Renske")
   })
 app.listen(port, () => console.log(`The server is listening on port ${port}`))
